58d6dff70ffa7e0a7becd6421472b245fda59a65
Salim <salimsalaues@gmail.com>
2017-09-18 08:18:05 -0700

    Keras with tensorflow backend

---
+++ ai/seq.py
@@ -0,0 +1,48 @@
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import train_test_split
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.optimizers import SGD
+from keras.layers import Dense
+from keras.utils import np_utils
+from imutils import paths
+import numpy as np
+import argparse
+import cv2
+import os
+
+def image_to_feature_vector(image, size=(32, 32)):
+	# resize the image to a fixed size, then flatten the image into
+	# a list of raw pixel intensities
+	return cv2.resize(image, size).flatten()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+args = vars(ap.parse_args())
+ 
+# grab the list of images that we'll be describing
+print("[INFO] describing images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+ 
+# initialize the data matrix and labels list
+data = []
+labels = []
+
+#  loop over the input images
+for (i, imagePath) in enumerate(imagePaths):
+	# load the image and extract the class label (assuming that our
+	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
+	image = cv2.imread(imagePath)
+	label = imagePath.split(os.path.sep)[-1].split(".")[0]
+ 
+	# construct a feature vector raw pixel intensities, then update
+	# the data matrix and labels list
+	features = image_to_feature_vector(image)
+	data.append(features)
+	labels.append(label)
+ 
+	# show an update every 1,000 images
+	if i > 0 and i % 1000 == 0:
+		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
